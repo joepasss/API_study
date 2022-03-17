8d08db12ab09ca1f6f423929f71a32840cae5cba
Miller <farwes1180@gmail.com>
2022-03-17 14:24:05 +0900

    node http module rest api reference

--- nodehttp/server.js
+++ nodehttp/server.js
@@ -7,12 +7,49 @@ const todos = [
 ];
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'X-Powerd-By': 'Node.js',
-  });
+  const { method, url } = req;
+  let body = [];
 
-  res.end(JSON.stringify({ success: true, data: todos }));
+  req
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+        error: null,
+      };
+
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+
+        // Validation
+        if (!id || !text) {
+          status = 400;
+          response.error = 'Please add id and text';
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powerd-By': 'Node.js',
+      });
+
+      res.end(JSON.stringify(response));
+    });
 });
 
 const PORT = 5000;
