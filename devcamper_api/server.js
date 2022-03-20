15c282309738110793fea610b868aa2298d3817f
Miller <farwes1180@gmail.com>
2022-03-21 05:01:47 +0900

    connect db(mysql) to server

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const { sequelize } = require('./models');
 
 dotenv.config({ path: './config/config.env' });
+
+// Connect db
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('MYSQL DATABASE connect success'.cyan.underline.bold);
+  })
+  .catch((err) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+  });
 
 // Route file
 const bootcamps = require('./routes/bootcamps');
@@ -20,7 +33,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
