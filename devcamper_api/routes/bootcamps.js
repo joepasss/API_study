b56168fcaacc3a057320937d317b9985d96fff77
Miller <farwes1180@gmail.com>
2022-03-24 22:18:26 +0900

    simplyfy router

--- devcamper_api/routes/bootcamps.js
+++ devcamper_api/routes/bootcamps.js
@@ -1,30 +1,20 @@
 const express = require('express');
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+router.route('/').get(getBootcamps).post(createBootcamp);
 
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'create new bootcamps' });
-});
-
-router.get('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `show single bootcamp ${req.params.id}` });
-});
-
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `delete bootcamp ${req.params.id}` });
-});
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
