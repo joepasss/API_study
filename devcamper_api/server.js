b1f0dc8879c563065fcd698c9f4e5a09fe4726b8
Miller <farwes1180@gmail.com>
2022-03-24 22:33:13 +0900

    connect db

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
 
 const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const app = express();
-
-app.use('/api/v1/bootcamps', bootcamps);
-
-const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.listen(
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+process.on('unhandledRejection', (err, promise) => {
+  console.err(`Error: ${err.message}`.red);
+});
