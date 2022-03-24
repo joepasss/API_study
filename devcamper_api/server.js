19b0ac281e351da2edecdbc6e7baa67a31743c08
Miller <farwes1180@gmail.com>
2022-03-24 22:19:45 +0900

    add morgan

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 const bootcamps = require('./routes/bootcamps');
 
@@ -10,6 +11,10 @@ const app = express();
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.listen(
   PORT,
